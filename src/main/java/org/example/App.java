e914e18cb3a23fb3519d5e7177352e1989d7d9ca
hannastigland <hanna.stigland@iths.se>
2023-09-04 12:18:18 +0200

    Updates to App.java Started with 1 and 2

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,8 +1,67 @@
 package org.example;
+import java.util.Scanner;
 
 public class App {
 
     public static void main(String[] args) {
-        System.out.println("Hello There!");
+        Scanner scanner = new Scanner(System.in);
+        char val;
+
+        int[] priser = new int[24];
+
+        do {
+            System.out.print("Elpriser\n");
+            System.out.print("========\n");
+            System.out.print("1. Inmatning\n");
+            System.out.print("2. Min, Max och Medel\n");
+            System.out.print("3. Sortera\n");
+            System.out.print("4. Bästa Laddningstid (4h)\n");
+            System.out.print("e. Avsluta\n");
+            val = scanner.next().charAt(0);
+
+            switch (Character.toLowerCase(val)) {
+                case '1':
+                    for (int timme = 0; timme < priser.length; timme++) {
+                        System.out.print("Skriv in priset för "+(timme+1)+":a timmen i hela ören. \n");
+                        priser[timme] = scanner.nextInt();
+                    }
+                    break;
+                case '2':
+                    int min = priser[0];
+                    int max = priser[0];
+                    int summa = priser[0];
+                    int timmeMin = 0;
+                    int timmeMax = 0;
+                    for (int timme = 1; timme < priser.length; timme++) {
+                        int aktuelltPris = priser[timme];
+
+                        if (aktuelltPris < min) {
+                            min = aktuelltPris;
+                            timmeMin = timme;
+                        }
+                        if (aktuelltPris > max) {
+                            max = aktuelltPris;
+                            timmeMax = timme;
+                        }
+                        summa += aktuelltPris;
+                    }
+                    int medel = summa / priser.length;
+
+                    System.out.print("Lägsta pris: " + formatTimme(timmeMin) + ", "+min+" öre per kW/h. \n");
+                    System.out.print("Högsta pris: " + formatTimme(timmeMax) + ", "+max+" öre per kW/h. \n");
+                    System.out.print("Genomsnittligt pris: "+medel+" öre per kW/h. \n");
+                    break;
+
+                case 'e':
+                    System.out.print("Programmet avslutas.");
+                    break;
+                default:
+                    System.out.print("Ej giltigt, försök igen.\n");
+            }
+
+        } while (Character.toLowerCase(val) != 'e');
+    }
+    public static String formatTimme(int timme) {
+        return String.format("%02d-%02d", timme, (timme + 1) % 24);
     }
 }
